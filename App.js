a99d8ae5415b83bcc8a5e44b976fdabf43e16bd6
alamgir.inu96 <alamgir.inu96@gmail.com>
2023-01-06 22:29:28 +0500

    creat focus feature

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { colors } from "./src/utils/colors";
+import { Focus } from "./src/features/Focus";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
+      <Text>
+        <Focus />
       </Text>
       <StatusBar style="auto" />
     </View>
@@ -19,8 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: colors.darkblue,
     alignItems: "center",
     justifyContent: "center",
-  },
-  text: {
-    color: colors.white,
   },
 });
